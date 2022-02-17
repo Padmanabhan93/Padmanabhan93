50725bea1fb8569fa9972af0f11c0ac43cf7e0e0
Padmanabhan93 <98248003+Padmanabhan93@users.noreply.github.com>
2022-02-17 08:29:51 +0530

    Add files via upload

---
+++ 20220217_while.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    int a=0,n;
+    printf("\nenter the limit numbers:\n");
+    scanf("%d",&n);
+    while(a<=n)
+    {
+        printf("%d\n",a);
+        a++;
+    }
+    return 0;
+}
